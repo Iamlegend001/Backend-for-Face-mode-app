7137556e78c34434108aff53d536f1daf3081f46
Arka <arkapravasantra17@gmail.com>
2025-07-18 21:57:58 +0530

    Full stack complete but i need to think about the ui and other errors

--- src/routes/song.routes.js
+++ src/routes/song.routes.js
@@ -1,25 +1,39 @@
-const express = require('express')
-const multer = require('multer')
-const uploadFile = require('../services/storage.service')
+const express = require("express");
+const multer = require("multer");
+const uploadFile = require("../services/storage.service");
+const songModel = require("../models/song.model");
 
-const router = express.Router()
+const router = express.Router();
 
+const upload = multer({ storage: multer.memoryStorage() });
 
-const upload = multer({storage:multer.memoryStorage()})
-
-router.post('/songs',upload.single('audio'),async (req,res)=>{
+router.post("/songs", upload.single("audio"), async (req, res) => {
   console.log(req.body);
   console.log(req.file);
   const fileData = await uploadFile(req.file);
-  console.log(fileData)
+
+  const song = await songModel.create({
+    title: req.body.title,
+    artist: req.body.artist,
+    audio: fileData.url,
+    mood: req.body.mood,
+  });
   res.status(201).json({
-    message: 'Song created successfully',
-    song: req.body
-  })
-})
-  
+    message: "Song created successfully",
+    song: song,
+  });
+});
 
+router.get("/songs", async (req, res) => {
+  const {mood} = req.query;
 
-
+  const songs = await songModel.find({
+    mood: mood,
+  });
+  res.status(200).json({
+    message: "Songs fetched successfully",
+    songs: songs,
+  });
+});
 
 module.exports = router;
